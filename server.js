715334c39d7150694b8e0a4af3c7278eca6c0bf3
Tsung Hung <hungmasterj@gmail.com>
2015-05-27 13:13:33 -0700

    compiled server

---
+++ server.js
@@ -0,0 +1,45 @@
+var Hapi = require('hapi');
+var path = require('path');
+var firebase = require('firebase');
+var server = new Hapi.Server();
+server.connection({
+    port: 3000,
+    host: "localhost"
+});
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    path: path.join(__dirname, 'public')
+});
+server.route({
+    path: '/{param*}',
+    method: 'GET',
+    handler: {
+        directory: {
+            path: "./public",
+            index: true
+        }
+    }
+});
+server.register([
+    {
+        register: require('good'),
+        options: {
+            reporters: [{
+                    reporter: require('good-console'),
+                    events: { response: '*' }
+                }]
+        }
+    }
+], function (err) {
+    if (err) {
+        throw err;
+    }
+    server.start(function (err) {
+        if (err) {
+            console.log("Something went wrong: " + err);
+        }
+        console.log('Hapi Server is running on port 3000!');
+    });
+});
