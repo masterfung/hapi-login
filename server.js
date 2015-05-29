74f79ecf1fab791873d1fdc21352e4e44def33fa
Tsung Hung <hungmasterj@gmail.com>
2015-05-28 18:00:40 -0700

    updated and moved routes out to its own while adding config ignore

--- server.js
+++ server.js
@@ -1,77 +1,55 @@
 var Hapi = require('hapi');
-var path = require('path');
-var Joi = require('Joi');
-var firebase = require('./firebase');
+var Good = require('good');
+var Routes = require('./routes');
+var Config = require('./config');
+// var User = require('./models/user').User;
+
+// Create a server with a host and port
 var server = new Hapi.Server();
+server.connection({ port: Config.server.port });
 
-server.connection({
-    port: 3000,
-    host: "localhost"
-});
-
-server.views({
-    engines: {
-        html: require('handlebars')
-    },
-    path: path.join(__dirname, 'public')
-});
-
-server.route({
-    path: '/{param*}',
-    method: 'GET',
-    handler: {
-        directory: {
-            path: "./public",
-            index: true
-        }
-    }
-});
-
-server.route({
-    path: '/login/{param*}',
-    method: 'GET',
-    handler: {
-        directory: {
-            path: path.join(__dirname, 'public/templates/login/'),
-            index: true
-        }
-    }
-});
-
-server.route({
-    path: '/login/{param*}',
-    method: 'POST',
-    config: {
-      handler: function(req, res) {
-
-      },
-      validate: {
-          payload: {
-            email: Joi.string().email()
-            password: Joi.string().alphanum(),.min(8)
-          }
-      }
-    }
-});
-
-server.register([
-    {
-        register: require('good'),
-        options: {
-            reporters: [{
-                    reporter: require('good-console'),
-                    events: { response: '*' }
-                }]
-        }
-    }
-], function (err) {
+// Register the plugin
+server.register(require('hapi-auth-cookie'), function (err) {
     if (err) {
         throw err;
     }
-    server.start(function (err) {
-        if (err) {
-            console.log("Something went wrong: " + err);
-        }
-        console.log('Hapi Server is running on port 3000!');
+
+    // Set our strategy
+    server.auth.strategy('session', 'cookie', {
+        password: 'thepursuitofhapiness', // cookie secret
+        cookie: 'session', // Cookie name
+        isSecure: false, // required for non-https applications
+        ttl: 24* 60 * 60 * 1000 // Set session to 1 day
+    });
+});
+
+// Print some information about the incoming request for debugging purposes
+server.ext('onRequest', function (request, reply) {
+    console.log(request.path, request.query);
+    return reply.continue();
+});
+
+//Routes
+server.route(Routes.endpoints);
+
+// Good logging & Start the server
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
     });
 });
