abf3ba5925e5b290e2c856060bf3e8858af2f4a9
Tsung Hung <hungmasterj@gmail.com>
2015-05-29 17:21:48 -0700

    fixed the assets loading issue by adding directory public route

--- routes.js
+++ routes.js
@@ -2,16 +2,16 @@ var Pages = require('./controllers/pages');
 var Authentication = require('./controllers/authentication');
 
 /**
- * Contains the list of all routes, i.e. methods, paths and the config functions
- * that take care of the actions
+ * Contains the list of all routes and associate methods in Pages import
  */
 exports.endpoints = [
 	{ method: 'GET',    path: '/',               config: Pages.index    },
 	{ method: 'GET',    path: '/login',          config: Pages.login    },
-	{ method: 'GET',    path: '/signup',       config: Pages.signup },
+	{ method: 'GET',    path: '/signup',         config: Pages.signup },
 	{ method: 'GET',    path: '/dashboard',      config: Pages.secret   },
 
 	{ method: 'POST',   path: '/login',          config: Authentication.login },
 	{ method: 'GET',    path: '/logout',         config: Authentication.logout },
-	{ method: 'POST',   path: '/signup',       config: Authentication.signup },
+	{ method: 'POST',   path: '/signup',         config: Authentication.signup },
+	{ method: 'GET',    path: '/{param*}',       config: Pages.assets }
 ];
